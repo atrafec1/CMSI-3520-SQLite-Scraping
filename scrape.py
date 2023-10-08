c7d896deebfdb2e59dc127af5153f357bdc01d24
Adam Trafecanty <atrafec1@lion.lmu.edu>
2023-10-07 17:55:56 -0700

    Scrape

--- scrape.py
+++ scrape.py
@@ -13,16 +13,26 @@ rank = rank[:333]
 td = browser.page.find_all("td")
 columns = [value.text.replace("\n", "") for value in td]
 columns = columns[18:3348]
-print(columns)
-print(columns[len(columns)-1])
+
+
+
+
+
+
+
+#3348
+
+
 
 column_names = ["City",
                 "State",
-                "2022_Estimate",
-                "2022_Census", 
+                "The_2022_Estimate",
+                "The_2022_Census", 
                 "Change", 
-                "2020_Land_Area", 
-                "2020_Population_Density", 
+                "The_2020_Land_Area",
+                "not_needed",
+                "The_2020_Population_Density",
+                "not_needed2",
                 "Location" 
                 ]
 
@@ -30,21 +40,26 @@ column_names = ["City",
 # # column[1:][::11]
 # # column[2:][::11]
 
-dictionary = {"2022_Rank": rank}
+dictionary = {"The_2022_Rank": rank}
 
 for idx, key in enumerate(column_names):
-    dictionary[key] = columns[idx:][::8]
+    dictionary[key] = columns[idx:][::10]
+
+dictionary.pop("not_needed")
+dictionary.pop("not_needed2")
+column_names.remove("not_needed")
+column_names.remove("not_needed2")
+
 
 
 df = pd.DataFrame(data = dictionary)
-# print(df.head())
-# print(df.tail())
 
 connection = sqlite3.connect("distro.db")
 cursor = connection.cursor()
-cursor.execute("create table linux (Distribution, " + ",".join(column_names) + ")")
+cursor.execute("DROP TABLE IF EXISTS linux")
+cursor.execute("create table linux (Rank, " + ",".join(column_names) + ")")
 for i in range(len(df)):
-    cursor.execute("insert into linux values (?,?,?,?,?,?,?,?,?,?,?,?)", df.iloc[i])
+    cursor.execute("insert into linux values (?,?,?,?,?,?,?,?,?)", df.iloc[i])
 
 connection.commit()
 
